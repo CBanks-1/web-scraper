93975bbaf707e078946b100a50c518e122f9daec
Curtis Banks <102325476+CBanks-1@users.noreply.github.com>
2023-01-17 16:10:54 -0800

    commit quotes for scraping

--- web scraper/scraper.py
+++ web scraper/scraper.py
@@ -1,8 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-req = requests.get("https://www.geeksforgeeks.org/")
 
-content = BeautifulSoup(req.content, "html.parser")
+scrape = requests.get("https://quotes.toscrape.com/",
+timeout = 5)
 
-print(content.prettify())
+req = BeautifulSoup(scrape.text, "html.parser")
+
+names = req.findAll("small", attrs={"class":"author"})
+quotes = req.findAll("span", attrs={"class":"text"})
+
+
+
+
+for quote in quotes:
+    print(quote.text)
+
+for name in names:
+    print(name.text)
